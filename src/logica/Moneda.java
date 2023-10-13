39e30bec49298c0de6640fea078dfcb253a4598c
JorgeDevmm <jorge29luis10@gmail.com>
2023-10-13 00:26:03 -0500

    Agregando clase Moneda y función calcularOperacion()

--- src/logica/Moneda.java
+++ src/logica/Moneda.java
@@ -1,16 +1,44 @@
 package logica;
 
+import javax.swing.*;
+
 /**
  * @author Jorge Monzón
  * @project personal-conversor-monedas
  */
 public class Moneda {
 
-    private String nombreMoneda;
+    private String tipo;
+    private Double valor;
 
 
-    public Moneda(String tipo, String valor) {
+    public Moneda(String tipo, Double valor) {
+        this.tipo = tipo;
+        this.valor = valor;
+    }
 
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String calcularMonto(double montoIngresado) {
+        Double resultado = montoIngresado * this.getValor();
+//      aplicar formato tipo string a dos decimales
+        String resultadoFormateado = String.format("%.2f", resultado);
+        String moneda = this.getTipo();
+        return resultadoFormateado;
     }
 
 }
--- src/logica/Test.java
+++ src/logica/Test.java
@@ -8,9 +8,7 @@ import javax.swing.*;
  */
 public class Test {
     public static void main(String[] args) {
-
-        Double resultado;
-        String resultaodFormateado, moneda;
+        Double monto;
         Boolean continuar = true;
 
 
@@ -18,99 +16,25 @@ public class Test {
 //      comboBox de opción
         JComboBox<String> comboBox = new JComboBox<>(opcionConvertir);
 
-
         do {
             String montoString = JOptionPane.showInputDialog("Ingresar el monto a convertir");
+
 
             if (montoString != null) {
 
                 try {
 
-                    Double monto = Double.parseDouble(montoString);
+                    monto = Double.parseDouble(montoString);
 
                     int opcion = JOptionPane.showConfirmDialog(null, comboBox, "Eliga la moneda a convertir", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+
 
 //      validar la opción escogida
                     if (opcion == JOptionPane.OK_OPTION) {
 //            capturar el índice del comboBox
                         int indiceSeleccinado = comboBox.getSelectedIndex();
 
-                        switch (indiceSeleccinado) {
-                            case 0:
-
-                                resultado = monto * 0.26;
-//                  convertir a dos decimales
-                                resultaodFormateado = String.format("%.2f", resultado);
-                                moneda = "Dolar";
-                                JOptionPane.showMessageDialog(null, "Tienes " + resultaodFormateado + " de " + moneda);
-                                break;
-                            case 1:
-                                resultado = monto * 0.25;
-//                  convertir a dos decimales
-                                resultaodFormateado = String.format("%.2f", resultado);
-                                moneda = "Euro";
-                                JOptionPane.showMessageDialog(null, "Tienes " + resultaodFormateado + " de " + moneda);
-                                break;
-                            case 2:
-                                resultado = monto * 0.21;
-//                  convertir a dos decimales
-                                resultaodFormateado = String.format("%.2f", resultado);
-                                moneda = "Libras Esterlinas";
-                                JOptionPane.showMessageDialog(null, "Tienes " + resultaodFormateado + " de " + moneda);
-                                break;
-                            case 3:
-                                resultado = monto * 38.91;
-//                  convertir a dos decimales
-                                resultaodFormateado = String.format("%.2f", resultado);
-                                moneda = "Yen japonés";
-                                JOptionPane.showMessageDialog(null, "Tienes " + resultaodFormateado + " de " + moneda);
-                                break;
-                            case 4:
-                                resultado = monto * 350.65;
-//                  convertir a dos decimales
-                                resultaodFormateado = String.format("%.2f", resultado);
-                                moneda = "Won surcoreano";
-                                JOptionPane.showMessageDialog(null, "Tienes " + resultaodFormateado + " de " + moneda);
-                                break;
-                            case 5:
-                                resultado = monto * 3.85;
-//                  convertir a dos decimales
-                                resultaodFormateado = String.format("%.2f", resultado);
-                                moneda = "Soles";
-                                JOptionPane.showMessageDialog(null, "Tienes " + resultaodFormateado + " de " + moneda);
-                                break;
-                            case 6:
-                                resultado = monto * 4.06;
-//                  convertir a dos decimales
-                                resultaodFormateado = String.format("%.2f", resultado);
-                                moneda = "Soles";
-                                JOptionPane.showMessageDialog(null, "Tienes " + resultaodFormateado + " de " + moneda);
-                                break;
-                            case 7:
-                                resultado = monto * 4.70;
-//                  convertir a dos decimales
-                                resultaodFormateado = String.format("%.2f", resultado);
-                                moneda = "Soles";
-                                JOptionPane.showMessageDialog(null, "Tienes " + resultaodFormateado + " de " + moneda);
-                                break;
-                            case 8:
-                                resultado = monto * 0.026;
-//                  convertir a dos decimales
-                                resultaodFormateado = String.format("%.2f", resultado);
-                                moneda = "Soles";
-                                JOptionPane.showMessageDialog(null, "Tienes " + resultaodFormateado + " de " + moneda);
-                                break;
-                            case 9:
-                                resultado = monto * 0.0029;
-//                  convertir a dos decimales
-                                resultaodFormateado = String.format("%.2f", resultado);
-                                moneda = "Soles";
-                                JOptionPane.showMessageDialog(null, "Tienes " + resultaodFormateado + " de " + moneda);
-                                break;
-                            default:
-                                System.out.println("opción no valida");
-                        }
-
+                        calcularOperacion(indiceSeleccinado, monto);
 
                     } else if (opcion == JOptionPane.CANCEL_OPTION) {
                         System.out.println("No selecciono acción o cancelo");
@@ -134,8 +58,6 @@ public class Test {
 
             int mensaje = JOptionPane.showConfirmDialog(null, "¿Desea continuar?", "Mensaje", JOptionPane.OK_OPTION, JOptionPane.QUESTION_MESSAGE);
 
-            System.out.println(mensaje);
-
 //            validación si desea repetir el proceso
             if (mensaje == 0) {
                 continuar = true;
@@ -147,6 +69,53 @@ public class Test {
         } while (continuar);
 
 
+    }
+
+    public static void calcularOperacion(int indiceSeleccinado, Double monto) {
+        switch (indiceSeleccinado) {
+            case 0:
+                Moneda dolar = new Moneda("Dolar", 0.26);
+                JOptionPane.showMessageDialog(null, "Tienes " + dolar.calcularMonto(monto) + " de " + dolar.getTipo());
+                break;
+            case 1:
+                Moneda euro = new Moneda("Euro", 0.25);
+                JOptionPane.showMessageDialog(null, "Tienes " + euro.calcularMonto(monto) + " de " + euro.getTipo());
+                break;
+            case 2:
+                Moneda librasEsterlinas = new Moneda("Libras Esterlinas", 0.21);
+                JOptionPane.showMessageDialog(null, "Tienes " + librasEsterlinas.calcularMonto(monto) + " de " + librasEsterlinas.getTipo());
+                break;
+            case 3:
+                Moneda yesJapones = new Moneda("Yen Japonés", 38.91);
+                JOptionPane.showMessageDialog(null, "Tienes " + yesJapones.calcularMonto(monto) + " de " + yesJapones.getTipo());
+                break;
+            case 4:
+                Moneda wonSurcoreano = new Moneda("Won surcoreano", 350.65);
+                JOptionPane.showMessageDialog(null, "Tienes " + wonSurcoreano.calcularMonto(monto) + " de " + wonSurcoreano.getTipo());
+                break;
+            case 5:
+                Moneda dolarSoles = new Moneda("Soles", 3.85);
+                JOptionPane.showMessageDialog(null, "Tienes " + dolarSoles.calcularMonto(monto) + " de " + dolarSoles.getTipo());
+                break;
+            case 6:
+                Moneda euroSoles = new Moneda("Soles", 4.06);
+                JOptionPane.showMessageDialog(null, "Tienes " + euroSoles.calcularMonto(monto) + " de " + euroSoles.getTipo());
+                break;
+            case 7:
+                Moneda librasSoles = new Moneda("Soles", 4.70);
+                JOptionPane.showMessageDialog(null, "Tienes " + librasSoles.calcularMonto(monto) + " de " + librasSoles.getTipo());
+                break;
+            case 8:
+                Moneda yenSoles = new Moneda("Soles", 0.026);
+                JOptionPane.showMessageDialog(null, "Tienes " + yenSoles.calcularMonto(monto) + " de " + yenSoles.getTipo());
+                break;
+            case 9:
+                Moneda wonSoles = new Moneda("Soles", 0.0029);
+                JOptionPane.showMessageDialog(null, "Tienes " + wonSoles.calcularMonto(monto) + " de " + wonSoles.getTipo());
+                break;
+            default:
+                System.out.println("opción no valida");
+        }
     }
 
 }
